81a52bb06a57e50762769d08898b6a2aa76a678f
Milan-C-I <milancheriyamanep@gmail.com>
2024-11-21 22:35:32 +0530

    fetch functions done

--- backend/mongodb.ts
+++ backend/mongodb.ts
@@ -15,17 +15,78 @@ async function connect() {
     }
     return clientPromise;
 }
-export async function getCharacters(){
+export async function getCharacters() {
+    try {
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Mondstadt"}).toArray()));
+        return characters;
+    } catch (error) {
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersByMondstadt(){
     try{
         const client = await connect();
         const db = client.db("genshin-web-info");
         const collection = db.collection("characters");
-        // const characters = await collection.find({region: "Liyue"}).toArray();
-        // return characters.map((char) => ({
-        //     ...char,
-        //     _id: char._id.toString(),
-        // }));
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Mondstadt"}).toArray()));
+        return characters
+    }catch(error){
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersByLiyue(){
+    try{
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Liyue"}).toArray()));
+        return characters
+    }catch(error){
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersByInazuma(){
+    try{
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Inazuma"}).toArray()));
+        return characters
+    }catch(error){
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersBySumeru(){
+    try{
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
         const characters = JSON.parse(JSON.stringify(await collection.find({region: "Sumeru"}).toArray()));
+        return characters
+    }catch(error){
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersByFontaine(){
+    try{
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Fontaine"}).toArray()));
+        return characters
+    }catch(error){
+        console.error("Error fetching characters:", error);
+    }
+}
+export async function getCharactersByNatlan(){
+    try{
+        const client = await connect();
+        const db = client.db("genshin-web-info");
+        const collection = db.collection("characters");
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Natlan"}).toArray()));
         return characters
     }catch(error){
         console.error("Error fetching characters:", error);
