93e755d9adb7f8b61ecb3739b4ede942684d633d
Milan-C-I <milancheriyamanep@gmail.com>
2024-11-21 19:21:44 +0530

    card fixed

--- app/charCard.tsx
+++ app/charCard.tsx
@@ -51,6 +51,7 @@ export default function CharCard({char}:{char?:any}) {
                 
                 ></Image>
             </div>
+            <div className="charDetails">
             <h1 className={`charName ${fontName.className}`}
             style={{
                 opacity: isVisible ? 1 : 0 ,
@@ -68,9 +69,8 @@ export default function CharCard({char}:{char?:any}) {
             }}
             >
                 {char?.description}
-                {/* Arlecchino, also known by her codename "The Knave," is a playable Pyro character in Genshin Impact.
-She is the Fourth of the Eleven Fatui Harbingers and the current head of the House of the Hearth. She is addressed as "Father" by members of the House, who she calls her "children." */}
-                </p>
+            </p>
+            </div>
         </div>
     )
 }
--- backend/mongodb.ts
+++ backend/mongodb.ts
@@ -5,7 +5,7 @@ let client: MongoClient;
 
 async function connect() {
     if (!client) {  
-        client = new MongoClient(process.env.MONGO_URI as string);
+        client = new MongoClient(process.env.MONGO_URI!);
         await client.connect();
     }
     return client;
@@ -15,25 +15,14 @@ export async function getCharacters(){
         const client = await connect();
         const db = client.db("genshin-web-info");
         const collection = db.collection("characters");
-        const characters = await collection.find({region: "Liyue"}).toArray();
-        return characters.map((char) => ({
-            ...char,
-            _id: char._id.toString(), // Convert ObjectId to string
-        }));
+        // const characters = await collection.find({region: "Liyue"}).toArray();
+        // return characters.map((char) => ({
+        //     ...char,
+        //     _id: char._id.toString(),
+        // }));
+        const characters = JSON.parse(JSON.stringify(await collection.find({region: "Liyue"}).toArray()));
+        return characters
     }catch(error){
         console.error("Error fetching characters:", error);
     }
 }
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//     try {
-//         const client = await connect();
-//         const db = client.db("genshin-web-info");
-//         const collection = db.collection("characters");
-//         const characters = await collection.find({}).toArray();
-//         res.status(200).json(characters);
-//     } catch (error) {
-//         console.error("Error fetching characters:", error);
-//         res.status(500).json({ message: "Internal Server Error" });
-//     }
-// }
