0a5c9cfc806b61a683daa6b04c169043a6c0543b
Milan-C-I <milancheriyamanep@gmail.com>
2024-12-07 23:11:20 +0530

    fetching from backend

--- app/chardet/page.tsx
+++ app/chardet/page.tsx
@@ -1,12 +1,12 @@
-import { getCharacterByName, getCharactersByRegion } from "@/backend/mongodb";
+import { getAllDetailsCharacterByName, getAllDetailsCharactersByRegion } from "@/backend/mongodb";
 import CharDetails from "./charDetails";
 
 export default async function characterDetails({ searchParams }: { searchParams: { char: string } }) {
   const searchParamsObject = await searchParams;
 
   const char = searchParamsObject.char;
-  const character = await getCharacterByName(char);
-  const region = await getCharactersByRegion(character?.region);
+  const character = await getAllDetailsCharacterByName(char);
+  const region = await getAllDetailsCharactersByRegion(character?.region);
   const index = region?.findIndex((c: any) => c.name === char);
   return (
     <div>
